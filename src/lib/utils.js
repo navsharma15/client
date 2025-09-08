c5f0fd8aead2c22c710aba81fac811c230ac2bb6
Nav Sharma <navdhruv12@gmail.com>
2025-09-08 18:43:38 +0530

    add login page

---
+++ src/lib/utils.js
@@ -0,0 +1,7 @@
+export function formatMessageTime(date) {
+  return new Date(date).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false, // 24-hour format, set to true for AM/PM
+  });
+}
